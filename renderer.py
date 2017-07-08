05961d8496bba19eac8939db228744a635de6a2a
miyuchina <miyu.china@icloud.com>
2017-07-08 15:49:57 +0800

    💡 support reading from stdin

--- renderer.py
+++ renderer.py
@@ -1,16 +1,20 @@
 import sys
 import parser
 
-def render_file(filename):
-    with open(filename, 'r') as fin:
-        lines = fin.readlines()
-        rendered_l = [ token.render() for token in parser.tokenize(lines) ]
-        rendered = ''.join(rendered_l)
-        rendered += '\n'
+def render_file(filename=""):
+    fin = open(filename, 'r') if filename else sys.stdin
+    lines = fin.readlines()
+    rendered_l = [ token.render() for token in parser.tokenize(lines) ]
+    rendered = ''.join(rendered_l)
+    rendered += '\n'
+    fin.close()
     return(rendered)
 
 if __name__ == "__main__":
     try:
         print(render_file(sys.argv[1]))
     except IndexError:
-        sys.exit('Not enough arguments.')
+        try:
+            print(render_file())
+        except KeyboardInterrupt:
+            sys.exit('Aborted by user.')
