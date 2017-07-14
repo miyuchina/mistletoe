bc270c70aeb95435b5800127f8a5d169b43099d9
miyuchina <miyu.china@icloud.com>
2017-07-14 20:15:19 +0800

    🐎 removed redundant matching in leaf_tokenizer

--- core/leaf_tokenizer.py
+++ core/leaf_tokenizer.py
@@ -7,28 +7,19 @@ class LeafTokenizer(object):
     def get_tokens(self):
         index = 0
         while index < len(self.content):
-            matched = 0
+            content = self.content[index:]
+            min_index = len(self.content)
+            min_match_obj = None
+            min_token_type = None
             for token_type in self.token_types:
-                match_obj = token_type.pattern.match(self.content[index:])
-                if match_obj:
-                    yield token_type(match_obj.group(1))
-                    matched = 1
-                    index += match_obj.end()
-                    break
-            if not matched:
-                min_index = len(self.content)
-                min_match_obj = None
-                min_token_type = None
-                for token_type in self.token_types:
-                    match_obj = token_type.pattern.search(self.content[index:])
-                    if match_obj and match_obj.start() < min_index:
-                        min_index = index + match_obj.start()
-                        min_match_obj = match_obj
-                        min_token_type = token_type
+                match_obj = token_type.pattern.search(content)
+                if match_obj and match_obj.start() < min_index:
+                    min_index = index + match_obj.start()
+                    min_match_obj = match_obj
+                    min_token_type = token_type
+            if min_index != index:
                 yield self.fallback_token(self.content[index:min_index])
-                if min_match_obj:
-                    yield min_token_type(min_match_obj.group(1))
-                    index += min_match_obj.end()
-                else:
-                    return
-        return
+            if min_match_obj:
+                yield min_token_type(min_match_obj.group(1))
+                index += min_match_obj.end()
+            else: return
