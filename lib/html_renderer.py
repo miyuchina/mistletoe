bb727346b25723c1545315fd01dc4514cb6c057f
miyuchina <miyu.china@icloud.com>
2017-07-18 19:23:46 +0800

    💡 added html_renderer support for most block_tokens
    Not supporting Tables currently.

--- lib/html_renderer.py
+++ lib/html_renderer.py
@@ -10,21 +10,37 @@ class HTMLRenderer(object):
             'Strikethrough': '<del>{_inner}</del>',
             'Image': '<img src="{src}" title="{title}" alt="{alt}">',
             'Link': '<a href="{target}">{_inner}</a>',
-            'EscapeSequence': '{_escape}'
+            'EscapeSequence': '{_escape}',
+
+            'Heading': '<h{level}>{_inner}</h{level}>',
+            'Quote': '<blockquote>{_inner}</blockquote>',
+            'Paragraph': '<p>{_inner}</p>',
+            'BlockCode': ('<pre><code class="lang-{language}">{_inner}'
+                          '</code></pre>'),
+            'List': '<{_tag}{_attr}>{_inner}</{_tag}>',
+            'ListItem': '<li>{_inner}</li>',
         }
 
     def render(self, token):
-        return self.render_span(token)
-
-    def render_span(self, token):
-        template = self.render_map[type(token).__name__]
-        if hasattr(token, 'children'): self.render_inner(token)
-        elif hasattr(token, 'content'): self.escape_content(token)
+        token_type = type(token).__name__
+        template = self.render_map[token_type]
+        if hasattr(token, 'children'): self.inner_helper(token)
+        if hasattr(token, 'content'): self.escape_helper(token)
+        if token_type == 'List': self.list_helper(token)
+        if token_type == 'TableCell': self.table_cell_helper(token)
         return template.format(**token.__dict__)
 
-    def render_inner(self, token):
-        inner = [ self.render_span(child) for child in token.children ]
+    def inner_helper(self, token):
+        inner = [ self.render(child) for child in token.children ]
         token._inner = ''.join(inner)
 
-    def escape_content(self, token):
+    def escape_helper(self, token):
         token._escape = html.escape(token.content)
+
+    def list_helper(self, token):
+        if hasattr(token, 'start'):
+            token._tag = 'ol'
+            token._attr = ' start="{}"'.format(token.start)
+        else:
+            token._tag = 'ul'
+            token._attr = ''
--- test/test_lib/test_html_renderer.py
+++ test/test_lib/test_html_renderer.py
@@ -43,3 +43,49 @@ class TestHTMLRenderer(unittest.TestCase):
     def test_raw_text(self):
         raw, target = 'john & jane', 'john &amp; jane'
         self._test_token(span_token.RawText, raw, target)
+
+    def test_heading(self):
+        raw, target = [ '### heading 3\n' ], '<h3>heading 3</h3>'
+        self._test_token(block_token.Heading, raw, target)
+
+    def test_quote(self):
+        raw = ['> ## heading 2\n', '> a paragraph\n', '> continued\n']
+        target = ('<blockquote><h2>heading 2</h2>'
+                  '<p>a paragraph continued</p></blockquote>')
+        self._test_token(block_token.Quote, raw, target)
+
+    def test_paragraph(self):
+        raw = ['a *paragraph*\n', 'with ~~multiple~~\n', 'lines\n']
+        target = ('<p>a <em>paragraph</em>'
+                  ' with <del>multiple</del> lines</p>')
+        self._test_token(block_token.Paragraph, raw, target)
+
+    def test_block_code(self):
+        raw = ['```sh\n', 'mkdir temp\n', 'rmdir temp\n', '```\n']
+        target = ('<pre><code class="lang-sh">mkdir temp\n'
+                  'rmdir temp\n</code></pre>')
+        self._test_token(block_token.BlockCode, raw, target)
+
+    def test_list(self):
+        raw = ['- item 1\n',
+               '- item 2\n',
+               '    2. nested item 1\n',
+               '    3. nested item 2\n',
+               '        * further nested\n',
+               '- item 3\n']
+        target = ('<ul>'
+                    '<li>item 1</li>'
+                    '<li>item 2</li>'
+                    '<ol start="2">'
+                      '<li>nested item 1</li>'
+                      '<li>nested item 2</li>'
+                      '<ul><li>further nested</li></ul>'
+                    '</ol>'
+                    '<li>item 3</li>'
+                  '</ul>')
+        self._test_token(block_token.List, raw, target)
+
+    def test_list_item(self):
+        raw = '    - some **bold** text\n'
+        target = '<li>some <strong>bold</strong> text</li>'
+        self._test_token(block_token.ListItem, raw, target)
