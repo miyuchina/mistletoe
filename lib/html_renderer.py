5e95249d093fe1625ce75085639f32eda096c98a
miyuchina <miyu.china@icloud.com>
2017-07-19 10:15:50 +0800

    ☕️ rewrote html_renderer for block_tokens

--- lib/html_renderer.py
+++ lib/html_renderer.py
@@ -3,14 +3,22 @@ import html
 class HTMLRenderer(object):
     def __init__(self):
         self.render_map = {
-                'Strong': self.render_strong,
-                'Emphasis': self.render_emphasis,
-                'InlineCode': self.render_inline_code,
-                'RawText': self.render_raw_text,
-                'Strikethrough': self.render_strikethrough,
-                'Image': self.render_image,
-                'Link': self.render_link,
+                'Strong':         self.render_strong,
+                'Emphasis':       self.render_emphasis,
+                'InlineCode':     self.render_inline_code,
+                'RawText':        self.render_raw_text,
+                'Strikethrough':  self.render_strikethrough,
+                'Image':          self.render_image,
+                'Link':           self.render_link,
                 'EscapeSequence': self.render_raw_text,
+                'Heading':        self.render_heading,
+                'Quote':          self.render_quote,
+                'Paragraph':      self.render_paragraph,
+                'BlockCode':      self.render_block_code,
+                'List':           self.render_list,
+                'ListItem':       self.render_list_item,
+                'Separator':      self.render_separator,
+                'Document':       self.render_document,
                 }
 
     def render(self, token):
@@ -32,12 +40,48 @@ class HTMLRenderer(object):
         return '<del>{}</del>'.format(self.render_inner(token))
 
     def render_image(self, token):
-        tp = '<img src="{src}" title="{title}" alt="{alt}">'
-        return tp.format(**token.__dict__)
+        tp = '<img src="{}" title="{}" alt="{}">'
+        return tp.format(token.src, token.title, token.alt)
 
     def render_link(self, token):
-        tp = '<a href="{target}">{inner}</a>'
-        return tp.format(target=token.target, inner=self.render_inner(token))
+        tp = '<a href="{}">{}</a>'
+        return tp.format(token.target, self.render_inner(token))
 
     def render_raw_text(self, token):
         return html.escape(token.content)
+
+    def render_heading(self, token):
+        tp = '<h{level}>{inner}</h{level}>'
+        return tp.format(level=token.level, inner=self.render_inner(token))
+
+    def render_quote(self, token):
+        tp = '<blockquote>{inner}</blockquote>'
+        return tp.format(inner=self.render_inner(token))
+
+    def render_paragraph(self, token):
+        return '<p>{}</p>'.format(self.render_inner(token))
+
+    def render_block_code(self, token):
+        tp = '<pre><code class="lang-{}">{}</code></pre>'
+        return tp.format(token.language, self.render_inner(token))
+
+    def render_list(self, token):
+        tp = '<{tag}{attr}>{inner}</{tag}>'
+        if hasattr(token, 'start'):
+            tag = 'ol'
+            attr = ' start="{}"'.format(token.start)
+        else:
+            tag = 'ul'
+            attr = ''
+        inner = self.render_inner(token)
+        return tp.format(tag=tag, attr=attr, inner=inner)
+
+    def render_list_item(self, token):
+        return '<li>{}</li>'.format(self.render_inner(token))
+
+    def render_separator(self, token):
+        return '<hr>'
+
+    def render_document(self, token):
+        tp = '<html><body>{inner}</body></html>'
+        return tp.format(inner=self.render_inner(token))
--- test/test_lib/test_html_renderer.py
+++ test/test_lib/test_html_renderer.py
@@ -43,3 +43,52 @@ class TestHTMLRenderer(unittest.TestCase):
     def test_raw_text(self):
         raw, target = 'john & jane', 'john &amp; jane'
         self._test_token(span_token.RawText, raw, target)
+
+    def test_heading(self):
+        raw, target = [ '### heading 3\n' ], '<h3>heading 3</h3>'
+        self._test_token(block_token.Heading, raw, target)
+
+    def test_quote(self):
+        raw = ['> ## heading 2\n', '> a paragraph\n', '> continued\n']
+        target = ('<blockquote><h2>heading 2</h2>'
+                  '<p>a paragraph continued</p></blockquote>')
+        self._test_token(block_token.Quote, raw, target)
+
+    def test_block_code(self):
+        raw = ['```sh\n', 'mkdir temp\n', 'rmdir temp\n', '```\n']
+        target = ('<pre><code class="lang-sh">mkdir temp\n'
+                  'rmdir temp\n</code></pre>')
+        self._test_token(block_token.BlockCode, raw, target)
+
+    def test_list(self):
+        raw = ['- item 1\n',
+               '- item 2\n',
+               '    2. nested item 1\n',
+               '    3. nested item 2\n',
+               '        * further nested\n',
+               '- item 3\n']
+        target = ('<ul>'
+                    '<li>item 1</li>'
+                    '<li>item 2</li>'
+                    '<ol start="2">'
+                      '<li>nested item 1</li>'
+                      '<li>nested item 2</li>'
+                      '<ul><li>further nested</li></ul>'
+                    '</ol>'
+                    '<li>item 3</li>'
+                  '</ul>')
+        self._test_token(block_token.List, raw, target)
+
+    def test_list_item(self):
+        raw = '    - some **bold** text\n'
+        target = '<li>some <strong>bold</strong> text</li>'
+        self._test_token(block_token.ListItem, raw, target)
+
+    def test_separator(self):
+        raw, target = '***\n', '<hr>'
+        self._test_token(block_token.Separator, raw, target)
+
+    def test_document(self):
+        raw = ['a paragraph\n']
+        target = '<html><body><p>a paragraph</p></body></html>'
+        self._test_token(block_token.Document, raw, target)
