a2462cf2c80595403c23cf1d515caf93d7a06f69
miyuchina <miyu.china@icloud.com>
2017-07-21 11:02:03 +0800

    💡 added support for html tokens

---
+++ mistletoe/html_tokenizer.py
@@ -0,0 +1,32 @@
+import re
+import mistletoe.span_token as span_token
+import mistletoe.block_token as block_token
+
+class HTMLBlock(block_token.BlockToken):
+    def __init__(lines):
+        self.content = ''.join(lines) # implicit newlines
+
+    @staticmethod
+    def match(lines):
+        open_tag_end = lines[0].find('>')
+        close_tag_start = lines[-1].find('</')
+        if (not lines[0].strip().startswith('<')
+            or open_tag_end == -1
+            or close_tag_start == -1):
+            return False
+        open_tag = lines[0][1:open_tag_end]
+        close_tag = lines[-1][close_tag_start+2:-2]
+        if open_tag != close_tag:
+            return False
+        return True
+
+class HTMLSpan(span_token.SpanToken):
+    pattern = re.compile(r"(<([A-z0-9]+)>(.+)<\/\2>)")
+    def __init__(self, content):
+        self.content = content
+
+span_token.HTMLSpan = HTMLSpan
+span_token.__all__.insert(0, 'HTMLSpan')
+
+block_token.HTMLBlock = HTMLBlock
+block_token.__all__.insert(0, 'HTMLBlock')
