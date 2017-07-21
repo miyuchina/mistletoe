0c1751737d02ca85cf4e1e0de744a54a4346b9b5
miyuchina <miyu.china@icloud.com>
2017-07-21 14:40:21 +0800

    💡 added plugin example: github wiki links

---
+++ plugins/github_wiki.py
@@ -0,0 +1,30 @@
+import re
+import mistletoe
+import mistletoe.html_renderer
+
+class GitHubWiki(mistletoe.span_token.SpanToken):
+    pattern = re.compile(r"(\[\[(.+?)\|(.+?)\]\])")
+    def __init__(self, raw):
+        alt, target = raw[2:-2].split('|', 1)
+        super().__init__(alt)
+        self.target = target
+
+class CustomRenderer(mistletoe.html_renderer.HTMLRenderer):
+    def __init__(self, preamble=''):
+        super().__init__(preamble)
+        self.render_map['GitHubWiki'] = self.render_github_wiki
+
+    def render_github_wiki(self, token):
+        template = '<a href="{}">{}</a>'
+        return template.format(token.target, self.render_inner(token))
+
+mistletoe.span_token.GitHubWiki = GitHubWiki
+mistletoe.span_token.__all__.append('GitHubWiki')
+
+if __name__ == '__main__':
+    lines = ['# GitHub Wiki link demo\n',
+             'A block-level token that\n',
+             'contains a [[Github Wiki|target]]\n',
+             'link.\n']
+    token = mistletoe.block_token.Document(lines)
+    print(CustomRenderer().render(token))
