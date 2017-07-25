2055e2ee4a092739475b4b205aeb56b9628ad695
miyuchina <miyu.china@icloud.com>
2017-07-25 22:31:45 +0800

    🐛 fixed: proper profiler functions

--- test/profiler/markdown_profiler.py
+++ test/profiler/markdown_profiler.py
@@ -1,8 +1,8 @@
-import markdown2
+import markdown
 
 def run():
     with open('jquery.md', 'r') as fin:
-        markdown2.markdown(fin.read())
+        markdown.markdown(fin.read())
 
 if __name__ == '__main__':
     for i in range(1000): run()
