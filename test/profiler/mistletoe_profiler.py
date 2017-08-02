9a86ac53ff5153ecdb62e5a6b46de2efc490a9d6
miyuchina <miyu.china@icloud.com>
2017-08-02 09:36:42 +0800

    💡 added Gruber's Markdown syntax document as test

--- test/profiler/mistletoe_profiler.py
+++ test/profiler/mistletoe_profiler.py
@@ -2,12 +2,16 @@ import mistletoe
 from mistletoe.html_token import Context
 from mistletoe.html_renderer import render
 from mistletoe import Document
+from time import time
 
 def run():
-    with open('test/profiler/jquery.md', 'r') as fin:
+    with open('test/profiler/syntax.md', 'r') as fin:
         with Context():
             rendered = render(Document(fin))
         return rendered
 
 if __name__ == '__main__':
+    start = time()
     for i in range(1000): run()
+    end = time()
+    print(end - start)
--- test/profiler/mistune_profiler.py
+++ test/profiler/mistune_profiler.py
@@ -1,8 +1,12 @@
 import mistune
+from time import time
 
 def run():
-    with open('jquery.md', 'r') as fin:
-        mistune.markdown(''.join(fin.readlines()))
+    with open('test/profiler/syntax.md', 'r') as fin:
+        mistune.markdown(fin.read())
 
 if __name__ == '__main__':
+    start = time()
     for i in range(1000): run()
+    end = time()
+    print(end - start)
