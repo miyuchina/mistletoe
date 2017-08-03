c5886fe41797b11998ae6ed594fcc9c1da130609
miyuchina <miyu.china@icloud.com>
2017-08-03 12:15:48 +0800

    ☕️ simplified mistletoe profiler

--- test/profiler/mistletoe_profiler.py
+++ test/profiler/mistletoe_profiler.py
@@ -1,14 +1,9 @@
 import mistletoe
-from mistletoe.html_token import Context
-from mistletoe.html_renderer import render
-from mistletoe import Document
 from time import time
 
 def run():
     with open('test/profiler/syntax.md', 'r') as fin:
-        with Context():
-            rendered = render(Document(fin))
-        return rendered
+        return mistletoe.markdown(fin)
 
 if __name__ == '__main__':
     start = time()
