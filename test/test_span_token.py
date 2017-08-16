04fe24893a54ed7f97ef499d811ccae6ae9b2707
miyuchina <miyu.china@icloud.com>
2017-08-16 12:15:21 +0800

    ☕️ refactored span_token tests: mock out RawText

--- test/test_span_token.py
+++ test/test_span_token.py
@@ -1,148 +1,100 @@
 import unittest
+from unittest.mock import patch
 import mistletoe.span_token as span_token
 
 
-class TestStrong(unittest.TestCase):
-    def test_parse_asterisk(self):
-        token = next(span_token.tokenize_inner('**some text**'))
-        self.assertIsInstance(token, span_token.Strong)
-        self.assertEqual(token._raw, '**some text**')
+class TestBranchToken(unittest.TestCase):
+    def setUp(self):
+        patcher = patch('mistletoe.span_token.RawText')
+        self.mock = patcher.start()
+        self.addCleanup(patcher.stop)
 
-    def test_parse_underscore(self):
-        token = next(span_token.tokenize_inner('__some text__'))
-        self.assertIsInstance(token, span_token.Strong)
-        self.assertEqual(token._raw, '__some text__')
+    def _test_parse(self, token_cls, raw, arg, **kwargs):
+        token = next(span_token.tokenize_inner(raw))
+        self.assertIsInstance(token, token_cls)
+        self._test_token(token, arg, **kwargs)
 
-
-class TestEmphasis(unittest.TestCase):
-    def test_parse_asterisk(self):
-        token = next(span_token.tokenize_inner('*some text*'))
-        self.assertIsInstance(token, span_token.Emphasis)
-        self.assertEqual(token._raw, '*some text*')
-
-    def test_parse_underscore(self):
-        token = next(span_token.tokenize_inner('_some text_'))
-        self.assertIsInstance(token, span_token.Emphasis)
-        self.assertEqual(token._raw, '_some text_')
+    def _test_token(self, token, arg, **kwargs):
+        for attr, value in kwargs.items():
+            self.assertEqual(getattr(token, attr), value)
+        next(token.children)
+        self.mock.assert_called_with(arg)
 
 
-class TestInlineCode(unittest.TestCase):
+class TestStrong(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('`some code`'))
-        self.assertIsInstance(token, span_token.InlineCode)
-        self.assertEqual(token._raw, '`some code`')
-
-    def test_children(self):
-        token = next(span_token.tokenize_inner('`some code`'))
-        child = next(token.children)
-        self.assertIsInstance(child, span_token.RawText)
-        self.assertEqual(child.content, 'some code')
+        self._test_parse(span_token.Strong, '**some text**', 'some text')
+        self._test_parse(span_token.Strong, '__some text__', 'some text')
 
 
-class TestStrikethrough(unittest.TestCase):
+class TestEmphasis(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('~~some code~~'))
-        self.assertIsInstance(token, span_token.Strikethrough)
-        self.assertEqual(token._raw, '~~some code~~')
+        self._test_parse(span_token.Emphasis, '*some text*', 'some text')
+        self._test_parse(span_token.Emphasis, '_some text_', 'some text')
 
 
-class TestLink(unittest.TestCase):
+class TestInlineCode(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('[name 1] (target 1)'))
-        self.assertIsInstance(token, span_token.Link)
-        self.assertEqual(token._raw, '[name 1] (target 1)')
-        self.assertEqual(token.target, 'target 1')
+        self._test_parse(span_token.InlineCode, '`some text`', 'some text')
+
+
+class TestStrikethrough(TestBranchToken):
+    def test_parse(self):
+        self._test_parse(span_token.Strikethrough, '~~some text~~', 'some text')
+
+
+class TestLink(TestBranchToken):
+    def test_parse(self):
+        self._test_parse(span_token.Link, '[name 1] (target 1)', 'name 1',
+                         target='target 1')
 
     def test_parse_multi_links(self):
         tokens = span_token.tokenize_inner('[n1](t1) & [n2](t2)')
+        self._test_token(next(tokens), 'n1', target='t1')
+        self._test_token(next(tokens), ' & ')
+        self._test_token(next(tokens), 'n2', target='t2')
 
-        token1 = next(tokens)
-        self.assertIsInstance(token1, span_token.Link)
-        self.assertEqual(token1._raw, '[n1](t1)')
-
-        token2 = next(tokens)
-        self.assertIsInstance(token2, span_token.RawText)
-        self.assertEqual(token2.content, ' & ')
-
-        token3 = next(tokens)
-        self.assertIsInstance(token3, span_token.Link)
-        self.assertEqual(token3._raw, '[n2](t2)')
-
-    def test_children(self):
+    def test_parse_children(self):
         token = next(span_token.tokenize_inner('[![alt](src)](target)'))
         child = next(token.children)
-        self.assertIsInstance(child, span_token.Image)
-        self.assertEqual(child._raw, '![alt](src)')
+        self._test_token(child, 'alt', src='src')
 
 
-class TestFootnoteLink(unittest.TestCase):
+class TestFootnoteLink(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('[alt] [key]'))
-        self.assertIsInstance(token, span_token.FootnoteLink)
-        self.assertEqual(token._raw, '[alt] [key]')
-
-    def test_footnote_anchor(self):
-        token = next(span_token.tokenize_inner('[alt] [key]')).target
-        self.assertIsInstance(token, span_token.FootnoteAnchor)
-        self.assertEqual(token.key, 'key')
+        with patch('mistletoe.span_token.FootnoteAnchor') as mock:
+            self._test_parse(span_token.FootnoteLink, '[alt] [key]', 'alt')
+            mock.assert_called_with('key')
 
 
-class TestAutoLink(unittest.TestCase):
+class TestAutoLink(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('<link>'))
-        self.assertIsInstance(token, span_token.AutoLink)
-        self.assertEqual(token._raw, '<link>')
-        self.assertEqual(token.name, 'link')
-        self.assertEqual(token.target, 'link')
+        self._test_parse(span_token.AutoLink, '<link>', 'link', target='link')
 
 
-class TestImage(unittest.TestCase):
-    def test_parse_without_title(self):
-        token = next(span_token.tokenize_inner('![alt] (link)'))
-        self.assertIsInstance(token, span_token.Image)
-        self.assertEqual(token._raw, '![alt] (link)')
-        self.assertEqual(token.alt, 'alt')
-        self.assertEqual(token.src, 'link')
-        self.assertIsNone(token.title)
-
-    def test_parse_with_title(self):
-        token = next(span_token.tokenize_inner('![alt] (link "title")'))
-        self.assertEqual(token.title, 'title')
-
-
-class TestFootnoteImage(unittest.TestCase):
+class TestImage(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('![alt] [key]'))
-        self.assertIsInstance(token, span_token.FootnoteImage)
-        self.assertEqual(token._raw, '![alt] [key]')
-        self.assertEqual(token.alt, 'alt')
-
-    def test_footnote_anchor(self):
-        token = next(span_token.tokenize_inner('![alt] [key]')).src
-        self.assertIsInstance(token, span_token.FootnoteAnchor)
-        self.assertEqual(token.key, 'key')
+        self._test_parse(span_token.Image, '![alt] (link)', 'alt', src='link')
+        self._test_parse(span_token.Image, '![alt] (link "title")', 'alt',
+                         src='link', title='title')
 
 
-class TestEscapeSequence(unittest.TestCase):
+class TestFootnoteImage(TestBranchToken):
     def test_parse(self):
-        token = next(span_token.tokenize_inner('\*'))
-        self.assertIsInstance(token, span_token.EscapeSequence)
-        self.assertEqual(token.content, '*')
+        with patch('mistletoe.span_token.FootnoteAnchor') as mock:
+            self._test_parse(span_token.FootnoteImage, '![alt] [key]', 'alt')
+            mock.assert_called_with('key')
+
+
+class TestEscapeSequence(TestBranchToken):
+    def test_parse(self):
+        self._test_parse(span_token.EscapeSequence, '\*', '*')
 
     def test_parse_in_text(self):
         tokens = span_token.tokenize_inner('some \*text*')
-
-        token1 = next(tokens)
-        self.assertIsInstance(token1, span_token.RawText)
-        self.assertEqual(token1.content, 'some ')
-
-        token2 = next(tokens)
-        self.assertIsInstance(token2, span_token.EscapeSequence)
-        self.assertEqual(token2.content, '*')
-
-        token3 = next(tokens)
-        self.assertIsInstance(token3, span_token.RawText)
-        self.assertEqual(token3.content, 'text*')
+        self._test_token(next(tokens), 'some ')
+        self._test_token(next(tokens), '*')
+        self._test_token(next(tokens), 'text*')
 
 
 class TestRawText(unittest.TestCase):
