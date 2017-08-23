91dbb8382cacb8e50f63766a5cfe3aec6476722b
miyuchina <miyu.china@icloud.com>
2017-08-23 18:32:37 +0800

    ☕️ refactored benchmark script

--- test/benchmark.py
+++ test/benchmark.py
@@ -2,65 +2,78 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import markdown
-import markdown2
-import mistune
-import mistletoe
+from importlib import import_module
 from time import perf_counter
 
-__all__ = ['run_markdown', 'run_markdown2', 'run_mistune', 'run_mistletoe']
 
 TEST_FILE = 'test/samples/syntax.md'
 TIMES = 1000
 
-def benchmark(func):
-    def inner():
-        print(func.__name__.split('_')[1], end=": ")
-        start = perf_counter()
-        for i in range(TIMES):
-            func()
-        end = perf_counter()
-        print(end - start)
-    return inner
 
-@benchmark
-def run_markdown():
+def benchmark(package_name):
+    def decorator(func):
+        def inner():
+            try:
+                package = import_module(package_name)
+            except ImportError:
+                return 'not available.'
+
+            start = perf_counter()
+            for i in range(TIMES):
+                func(package)
+            end = perf_counter()
+
+            return end - start
+        return inner
+    return decorator
+
+
+@benchmark('markdown')
+def run_markdown(package):
     with open(TEST_FILE, 'r') as fin:
-        return markdown.markdown(fin.read(), ['extra'])
+        return package.markdown(fin.read(), ['extra'])
 
-@benchmark
-def run_markdown2():
+
+@benchmark('markdown2')
+def run_markdown2(package):
     with open(TEST_FILE, 'r') as fin:
         extras = ['code-friendly', 'fenced-code-blocks', 'footnotes']
-        return markdown2.markdown(fin.read(), extras=extras)
+        return package.markdown(fin.read(), extras=extras)
 
-@benchmark
-def run_mistune():
-    with open(TEST_FILE, 'r') as fin:
-        return mistune.markdown(fin.read())
 
-@benchmark
-def run_mistletoe():
+@benchmark('mistune')
+def run_mistune(package):
     with open(TEST_FILE, 'r') as fin:
-        return mistletoe.markdown(fin)
+        return package.markdown(fin.read())
+
+
+@benchmark('mistletoe')
+def run_mistletoe(package):
+    with open(TEST_FILE, 'r') as fin:
+        return package.markdown(fin)
+
+
+def run(package_name):
+    print(package_name, end=': ')
+    print(globals()['run_{}'.format(package_name.lower())]())
+
+
+def run_all(package_names):
+    prompt = 'Running tests with {}...'.format(', '.join(package_names))
+    print(prompt)
+    print('='*len(prompt))
+    for package_name in package_names:
+        run(package_name)
+
 
 def main(*args):
     print('Test document: {}'.format(TEST_FILE))
     print('Test iterations: {}'.format(TIMES))
     if args[1:]:
-        runnables = ['run_{}'.format(arg) for arg in args[1:]]
-        prompt = 'Running tests with {}...'.format(', '.join(args[1:]))
-        print(prompt)
-        print('='*len(prompt))
-        for runnable in runnables:
-            globals()[runnable]()
+        run_all(args[1:])
     else:
-        packages = [key.split('_')[1] for key in __all__]
-        prompt = 'Running tests with {}...'.format(', '.join(packages))
-        print(prompt)
-        print('='*len(prompt))
-        for key in __all__:
-            globals()[key]()
+        run_all(['markdown', 'markdown2', 'mistune', 'mistletoe'])
+
 
 if __name__ == '__main__':
     main(*sys.argv)
