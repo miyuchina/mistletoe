a9c5c9d24ef0237a3c7beb4555593a0f93c45c53
miyuchina <miyu.china@icloud.com>
2017-11-25 15:31:36 -0500

    ☕️ refactored mocking in renderer tests

--- test/test_html_renderer.py
+++ test/test_html_renderer.py
@@ -9,86 +9,75 @@ class TestRenderer(TestCase):
         self.renderer.__enter__()
         self.addCleanup(self.renderer.__exit__, None, None, None)
 
-    def _test_token(self, token_path, output, children=True, **kwargs):
-        def mock_render(token_path):
-            def inner(token):
-                _, token_name = token_path.split('.')
-                return self.renderer.render_map[token_name](token)
-            return inner
-        self.renderer.render = mock_render(token_path)
-
-        path = 'mistletoe.{}'.format(token_path)
-        MockToken = mock.Mock(path, name=token_path)
-        if children:
-            MockToken.return_value = mock.Mock(children=mock.MagicMock(), **kwargs)
-        else:
-            MockToken.return_value = mock.Mock(**kwargs)
-
-        self.assertEqual(self.renderer.render(MockToken()), output)
+    def _test_token(self, token_name, output, children=True, **kwargs):
+        render_func = self.renderer.render_map[token_name]
+        children = mock.MagicMock() if children else None
+        mock_token = mock.Mock(children=children, **kwargs)
+        self.assertEqual(render_func(mock_token), output)
 
 
 class TestHTMLRenderer(TestRenderer):
     def test_strong(self):
-        self._test_token('span_token.Strong', '<strong>inner</strong>')
+        self._test_token('Strong', '<strong>inner</strong>')
 
     def test_emphasis(self):
-        self._test_token('span_token.Emphasis', '<em>inner</em>')
+        self._test_token('Emphasis', '<em>inner</em>')
 
     def test_inline_code(self):
-        self._test_token('span_token.InlineCode', '<code>inner</code>')
+        self._test_token('InlineCode', '<code>inner</code>')
 
     def test_strikethrough(self):
-        self._test_token('span_token.Strikethrough', '<del>inner</del>')
+        self._test_token('Strikethrough', '<del>inner</del>')
 
     def test_image(self):
         output = '<img src="src" title="title" alt="inner">'
-        self._test_token('span_token.Image', output, src='src', title='title')
+        self._test_token('Image', output, src='src', title='title')
 
     def test_link(self):
         output = '<a href="target">inner</a>'
-        self._test_token('span_token.Link', output, target='target')
+        self._test_token('Link', output, target='target')
 
     def test_autolink(self):
         output = '<a href="link">inner</a>'
-        self._test_token('span_token.AutoLink', output, target='link')
+        self._test_token('AutoLink', output, target='link')
 
     def test_escape_sequence(self):
-        self._test_token('span_token.EscapeSequence', 'inner')
+        self._test_token('EscapeSequence', 'inner')
 
     def test_raw_text(self):
-        self._test_token('span_token.RawText', 'john &amp; jane',
+        self._test_token('RawText', 'john &amp; jane',
                          children=False, content='john & jane')
 
     def test_html_span(self):
-        self._test_token('html_token.HTMLSpan', '<some>text</some>',
+        self._test_token('HTMLSpan', '<some>text</some>',
                          children=False, content='<some>text</some>')
 
     def test_heading(self):
         output = '<h3>inner</h3>\n'
-        self._test_token('block_token.Heading', output, level=3)
+        self._test_token('Heading', output, level=3)
 
     def test_quote(self):
         output = '<blockquote>\ninner</blockquote>\n'
-        self._test_token('block_token.Quote', output)
+        self._test_token('Quote', output)
 
     def test_paragraph(self):
-        self._test_token('block_token.Paragraph', '<p>inner</p>\n')
+        self._test_token('Paragraph', '<p>inner</p>\n')
 
     def test_block_code(self):
         output = '<pre>\n<code class="lang-sh">\ninner</code>\n</pre>\n'
-        self._test_token('block_token.BlockCode', output, language='sh')
+        self._test_token('BlockCode', output, language='sh')
 
     def test_block_code_no_language(self):
         output = '<pre>\n<code>\ninner</code>\n</pre>\n'
-        self._test_token('block_token.BlockCode', output, language='')
+        self._test_token('BlockCode', output, language='')
 
     def test_list(self):
         output = '<ul>\ninner</ul>\n'
-        self._test_token('block_token.List', output, start=None)
+        self._test_token('List', output, start=None)
 
     def test_list_item(self):
         output = '<li>inner</li>\n'
-        self._test_token('block_token.ListItem', output)
+        self._test_token('ListItem', output)
 
     def test_table_with_heading(self):
         func_path = 'mistletoe.html_renderer.HTMLRenderer.render_table_row'
@@ -98,32 +87,32 @@ class TestHTMLRenderer(TestRenderer):
                         '<thead>\nrow</thead>\n'
                         '<tbody>\ninner</tbody>\n'
                       '</table>\n')
-            self._test_token('block_token.Table', output, has_header=True)
+            self._test_token('Table', output, has_header=True)
 
     def test_table_without_heading(self):
         func_path = 'mistletoe.html_renderer.HTMLRenderer.render_table_row'
         with mock.patch(func_path, autospec=True) as mock_func:
             mock_func.return_value = 'row'
             output = '<table>\n<tbody>\ninner</tbody>\n</table>\n'
-            self._test_token('block_token.Table', output, has_header=False)
+            self._test_token('Table', output, has_header=False)
 
     def test_table_row(self):
-        self._test_token('block_token.TableRow', '<tr>\n</tr>\n')
+        self._test_token('TableRow', '<tr>\n</tr>\n')
 
     def test_table_cell(self):
         output = '<td align="left">inner</td>\n'
-        self._test_token('block_token.TableCell', output, align=None)
+        self._test_token('TableCell', output, align=None)
 
     def test_separator(self):
-        self._test_token('block_token.Separator', '<hr>\n', children=False)
+        self._test_token('Separator', '<hr>\n', children=False)
 
     def test_html_block(self):
         content = output = '<h1>hello</h1>\n<p>this is\na paragraph</p>\n'
-        self._test_token('html_token.HTMLBlock', output,
+        self._test_token('HTMLBlock', output,
                          children=False, content=content)
 
     def test_document(self):
-        self._test_token('block_token.Document', 'inner', footnotes={})
+        self._test_token('Document', 'inner', footnotes={})
 
 
 class TestHTMLRendererFootnotes(TestCase):
--- test/test_latex_renderer.py
+++ test/test_latex_renderer.py
@@ -10,98 +10,87 @@ class TestLaTeXRenderer(TestCase):
         self.renderer.__enter__()
         self.addCleanup(self.renderer.__exit__, None, None, None)
 
-    def _test_token(self, token_path, output, children=True, **kwargs):
-        def mock_render(token_path):
-            def inner(token):
-                _, token_name = token_path.split('.')
-                return self.renderer.render_map[token_name](token)
-            return inner
-        self.renderer.render = mock_render(token_path)
-
-        path = 'mistletoe.{}'.format(token_path)
-        MockToken = mock.Mock(path, name=token_path)
-        if children:
-            MockToken.return_value = mock.Mock(children=mock.MagicMock(), **kwargs)
-        else:
-            MockToken.return_value = mock.Mock(**kwargs)
-
-        self.assertEqual(self.renderer.render(MockToken()), output)
+    def _test_token(self, token_name, output, children=True, **kwargs):
+        render_func = self.renderer.render_map[token_name]
+        children = mock.MagicMock() if children else None
+        mock_token = mock.Mock(children=children, **kwargs)
+        self.assertEqual(render_func(mock_token), output)
 
     def test_strong(self):
-        self._test_token('span_token.Strong', '\\textbf{inner}')
+        self._test_token('Strong', '\\textbf{inner}')
 
     def test_emphasis(self):
-        self._test_token('span_token.Emphasis', '\\textit{inner}')
+        self._test_token('Emphasis', '\\textit{inner}')
 
     def test_inline_code(self):
-        self._test_token('span_token.InlineCode', '\\verb|inner|')
+        self._test_token('InlineCode', '\\verb|inner|')
 
     def test_strikethrough(self):
-        self._test_token('span_token.Strikethrough', '\\sout{inner}')
+        self._test_token('Strikethrough', '\\sout{inner}')
 
     def test_image(self):
         output = '\n\\includegraphics{src}\n'
-        self._test_token('span_token.Image', output, src='src')
+        self._test_token('Image', output, src='src')
 
     def test_link(self):
         output = '\\href{target}{inner}'
-        self._test_token('span_token.Link', output, target='target')
+        self._test_token('Link', output, target='target')
 
     def test_autolink(self):
         output = '\\url{target}'
-        self._test_token('span_token.AutoLink', output, target='target')
+        self._test_token('AutoLink', output, target='target')
 
     def test_math(self):
         output = '$ 1 + 2 = 3 $'
-        self._test_token('latex_token.Math', output,
+        self._test_token('Math', output,
                          children=False, content='$ 1 + 2 = 3 $')
 
     def test_raw_text(self):
         output = '\\$\\&\\#\\{\\}'
-        self._test_token('span_token.RawText', output,
+        self._test_token('RawText', output,
                          children=False, content='$&#{}')
 
     def test_heading(self):
         output = '\n\\section{inner}\n'
-        self._test_token('block_token.Heading', output, level=1)
+        self._test_token('Heading', output, level=1)
 
     def test_quote(self):
         output = '\\begin{displayquote}\ninner\\end{displayquote}\n'
-        self._test_token('block_token.Quote', output)
+        self._test_token('Quote', output)
 
     def test_paragraph(self):
         output = '\ninner\n'
-        self._test_token('block_token.Paragraph', output)
+        self._test_token('Paragraph', output)
 
     def test_block_code(self):
         output = '\n\\begin{lstlisting}[language=sh]\ninner\\end{lstlisting}\n'
-        self._test_token('block_token.BlockCode', output, language='sh')
+        self._test_token('BlockCode', output, language='sh')
 
     def test_list(self):
         output = '\\begin{itemize}\ninner\\end{itemize}\n'
-        self._test_token('block_token.List', output, start=None)
+        self._test_token('List', output, start=None)
 
     def test_list_item(self):
-        self._test_token('block_token.ListItem', '\\item inner\n')
+        self._test_token('ListItem', '\\item inner\n')
 
     def test_table_with_heading(self):
         output = '\\begin{tabular}{l c r}\n\n\\hline\ninner\\end{tabular}\n'
-        self._test_token('block_token.Table', output,
+        self._test_token('Table', output,
                          has_header=True, column_align=[None, 0, 1])
 
     def test_table_without_heading(self):
         output = ('\\begin{tabular}\ninner\\end{tabular}\n')
-        self._test_token('block_token.Table', output,
+        self._test_token('Table', output,
                          has_header=False, column_align=[None])
 
     def test_table_row(self):
-        self._test_token('block_token.TableRow', '\n')
+        self._test_token('TableRow', '\n')
 
     def test_table_cell(self):
-        self._test_token('block_token.TableCell', 'inner')
+        self._test_token('TableCell', 'inner')
 
     def test_separator(self):
-        self._test_token('block_token.Separator', '\\hrulefill\n')
+        self._test_token('Separator', '\\hrulefill\n')
 
     def test_document(self):
         output = ('\\documentclass{article}\n'
@@ -113,7 +102,7 @@ class TestLaTeXRenderer(TestCase):
                   '\\begin{document}\n'
                   'inner'
                   '\\end{document}\n')
-        self._test_token('block_token.Document', output, footnotes={})
+        self._test_token('Document', output, footnotes={})
 
 
 class TestLaTeXFootnotes(TestCase):
