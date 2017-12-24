035b580973b55252934adeda8e50ed926e5ebce4
miyuchina <miyu.china@icloud.com>
2017-12-24 10:30:51 +0800

    ☕️ refactored interactive mode: cleaner error handling

--- mistletoe/interactive.py
+++ mistletoe/interactive.py
@@ -1,35 +1,36 @@
 import sys
 import mistletoe
 
-try:
-    import readline
-except ImportError:
-    sys.stdout.write('[Warning] readline library not available.')
-
 
 def interactive():
     """
     Parse user input, dump to stdout, rinse and repeat.
     Python REPL style.
     """
-    print('mistletoe [version {}] (interactive)'.format(mistletoe.__version__))
-    print('Type Ctrl-D to complete input, or Ctrl-C to exit.')
+    _import_readline()
+    _print_heading()
     contents = []
     more = False
     while True:
         try:
-            if not more:
-                prompt = '>>> '
-                more = True
-            else:
-                prompt = '... '
-            try:
-                line = input(prompt) + '\n'
-                contents.append(line)
-            except EOFError:
-                print('\n' + mistletoe.markdown(contents), end='')
-                more = False
-                contents.clear()
+            prompt, more = ('... ', True) if more else ('>>> ', True)
+            contents.append(input(prompt) + '\n')
+        except EOFError:
+            print('\n' + mistletoe.markdown(contents), end='')
+            more = False
+            contents.clear()
         except KeyboardInterrupt:
             print('\nExiting.')
             break
+
+
+def _import_readline():
+    try:
+        import readline
+    except ImportError:
+        print('[warning] readline library not available.')
+
+
+def _print_heading():
+    print('mistletoe [version {}] (interactive)'.format(mistletoe.__version__))
+    print('Type Ctrl-D to complete input, or Ctrl-C to exit.')
