01befb7d924085aa82d9f85e2307d1a3ffeb4561
miyuchina <miyu.china@icloud.com>
2018-01-01 18:32:54 +0800

    🐛 fixed: splat operator usage unsupported

--- mistletoe/markdown_renderer.py
+++ mistletoe/markdown_renderer.py
@@ -6,16 +6,16 @@ class MarkdownRenderer(BaseRenderer):
     def __init__(self, *extras,
                  enabled_block_tokens=None, enabled_span_tokens=None):
         super().__init__(*extras)
-        self.all_block_tokens = [*block_token._token_types]
-        self.all_span_tokens = [*span_token._token_types]
+        self.all_block_tokens = block_token._token_types[:]
+        self.all_span_tokens = span_token._token_types[:]
         self.enabled_block_tokens = enabled_block_tokens or []
         self.enabled_span_tokens = enabled_span_tokens or []
         block_token._token_types = self.enabled_block_tokens
         span_token._token_types = self.enabled_span_tokens
 
     def __exit__(self, exception_type, exception_val, traceback):
-        block_token._token_types = [*self.all_block_tokens]
-        span_token._token_types = [*self.all_span_tokens]
+        block_token._token_types = self.all_block_tokens[:]
+        span_token._token_types = self.all_span_tokens[:]
 
     def render_raw_text(self, token):
         return token.content
