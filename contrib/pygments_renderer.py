8d962445d9e1baa70146dcec8e7931bd0db34cac
Mi Yu <hello@afteryu.me>
2018-04-25 18:02:59 -0400

    added style option for PygmentsRenderer

--- contrib/pygments_renderer.py
+++ contrib/pygments_renderer.py
@@ -1,13 +1,20 @@
-from mistletoe import HTMLRenderer, Document
+from mistletoe import HTMLRenderer
 from pygments import highlight
-from pygments.lexers import get_lexer_by_name, guess_lexer
+from pygments.styles import get_style_by_name as get_style
+from pygments.lexers import get_lexer_by_name as get_lexer, guess_lexer
 from pygments.formatters.html import HtmlFormatter
 
 class PygmentsRenderer(HTMLRenderer):
     formatter = HtmlFormatter()
     formatter.noclasses = True
 
+    def __init__(self, *extras, style='default'):
+        super().__init__(*extras)
+        self.formatter.style = get_style(style)
+
+
     def render_block_code(self, token):
         code = token.children[0].content
-        lexer = get_lexer_by_name(token.language) if token.language else guess_lexer(code)
+        lexer = get_lexer(token.language) if token.language else guess_lexer(code)
         return highlight(code, lexer, self.formatter)
+
