c1c633e87928054d6245db8adc7c0aa12124c307
Mi Yu <hello@afteryu.me>
2018-05-25 16:43:53 -0400

    freeze CommonMark tests at version 0.28

--- test/commonmark/spec.sh
+++ test/commonmark/spec.sh
@@ -3,13 +3,24 @@
 set -e
 
 REPO="https://github.com/commonmark/CommonMark.git"
+VERSION="0.28"
 
 function main {
-    git clone $REPO
+    echo "Cloning from repo: $REPO..."
+    git clone --quiet $REPO
+
+    echo "Using version $VERSION..."
     cd "CommonMark"
+    git checkout --quiet $VERSION
+
+    echo "Dumping tests file..."
     python3 "test/spec_tests.py" --dump-tests > "../commonmark.json"
+
+    echo "Cleaning up..."
     cd ..
     rm -rf CommonMark
+
+    echo "Done."
 }
 
 main
