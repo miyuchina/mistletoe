4d9ff1c87822d85166b519e14a1fb1a347b4d814
Mi Yu <miyu.china@icloud.com>
2018-05-30 12:41:30 -0400

    break LinkPattern.search into functions

--- mistletoe/span_util.py
+++ mistletoe/span_util.py
@@ -1,6 +1,10 @@
 import re
 
 
+angle_pattern = re.compile(r'\s*<([^\s<>]*?)>')
+title_pattern = re.compile(r'\s*(\".*?(?<!\\)\"|\'.*?(?<!\\)\'|\(.*?(?<!\\)\))?\s*')
+
+
 class _MatchObj:
     def __init__(self, fields, start, end):
         self.fields = fields
@@ -18,84 +22,102 @@ class _MatchObj:
 
 
 class LinkPattern:
-    angle_dest_pattern = re.compile(r'\s*<([^\s<>]*?)>')
-    title_pattern = re.compile(r'\s*(\".*?(?<!\\)\"|\'.*?(?<!\\)\'|\(.*?(?<!\\)\))?\s*\)')
-
     @classmethod
     def search(cls, string, start=0):
         # read link text
-        if start != 0:
-            string = string[start:]
-
-        text_start = string.find('[')
-        if text_start == -1:
+        text_pair = read_link_text(string, offset=start)
+        if text_pair is None:
             return None
-        brackets = 1
+        start, text_end, text = text_pair
+
+        # read dest
+        dest_pair = read_dest_text(string, offset=text_end)
+        if dest_pair is None:
+            return None
+        _, dest_end, dest = dest_pair
+
+        # read title
+        title_pair = read_link_title(string, offset=dest_end)
+        if title_pair is None:
+            return None
+        _, title_end, title = title_pair
+
+        # assert closing paren
+        if string[title_end] != ')':
+            return None
+        end = title_end + 1
+        return _MatchObj((text, dest, title), start, end)
+
+
+def read_link_text(string, offset=0):
+    if offset:
+        string = string[offset:]
+    start = string.find('[')
+    if start == -1:
+        return None
+    brackets = 1
+    escaped = False
+    for i, c in enumerate(string[start+1:], start=start+1):
+        if c == '\\':
+            escaped = True
+        elif not escaped:
+            if c == '[':
+                brackets += 1
+            if c == ']':
+                brackets -= 1
+        else:
+            escaped = False
+        if brackets == 0:
+            break
+    else:
+        return None
+    return offset+start, offset+i, string[start+1:i]
+
+
+def read_dest_text(string, offset=0):
+    if offset+2 >= len(string) or string[offset+1] != '(':
+        return None
+    start = offset + 2
+    match_obj = angle_pattern.match(string, pos=start)
+    if match_obj is not None:
+        end = match_obj.end()
+        dest = match_obj.group(1)
+    else:
+        start = skip_spaces(string, start)
+        parens = 1
         escaped = False
-        for i, c in enumerate(string[text_start+1:], start=text_start+1):
+        for i, c in enumerate(string[start:], start=start):
             if c == '\\':
                 escaped = True
             elif not escaped:
-                if c == '[':
-                    brackets += 1
-                if c == ']':
-                    brackets -= 1
+                if c == '(':
+                    parens += 1
+                if c == ')':
+                    parens -= 1
             else:
                 escaped = False
-            if brackets == 0:
+            if c == ' ' or parens == 0:
                 break
         else:
             return None
-        text_end = i
-        text = string[text_start+1:text_end]
+        end = i
+        dest = string[start:end]
+    return start, end, dest
 
-        # read dest
-        if text_end+2 >= len(string) or string[text_end+1] != '(':
-            return None
-        dest_start = text_end + 2
-        match_obj = cls.angle_dest_pattern.match(string, pos=dest_start)
-        if match_obj is not None:
-            dest_end = match_obj.end()
-            dest = match_obj.group(1)
-        else:
-            dest_start = cls.skip_spaces(string, dest_start)
-            parens = 1
-            escaped = False
-            for i, c in enumerate(string[dest_start:], start=dest_start):
-                if c == '\\':
-                    escaped = True
-                elif not escaped:
-                    if c == '(':
-                        parens += 1
-                    if c == ')':
-                        parens -= 1
-                else:
-                    escaped = False
-                if c == ' ':
-                    break
-                if parens == 0:
-                    return _MatchObj((text, string[dest_start:i], ''), start+text_start, start+i+1)
-            else:
-                return None
-            dest_end = i
-            dest = string[dest_start:dest_end]
 
-        # read title
-        match_obj = cls.title_pattern.match(string, pos=dest_end)
-        if match_obj is None:
-            return None
-        title = match_obj.group(1)
-        title_end = match_obj.end()
-        if title is not None:
-            title = title[1:-1]
-        else:
-            title = ''
+def read_link_title(string, offset=0):
+    match_obj = title_pattern.match(string, pos=offset)
+    if match_obj is None:
+        return None
+    title = match_obj.group(1)
+    end = match_obj.end()
+    if title is not None:
+        return offset, end, title[1:-1]
+    return offset, end, ''
 
-        return _MatchObj((text, dest, title), start+text_start, start+title_end+1)
 
-    @staticmethod
-    def skip_spaces(string, start=0):
-        for i, c in enumerate(string[start:], start=start):
-            if c != ' ' or c != '\n':
-                break
-        return i
+def skip_spaces(string, start=0):
+    for i, c in enumerate(string[start:], start=start):
+        if c != ' ' or c != '\n':
+            break
+    return i
