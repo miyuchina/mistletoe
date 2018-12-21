d80921c10e92145f721fec79e09363f3b86d4f11
Skye Im <skye.im@nyu.edu>
2018-12-21 17:53:59 -0500

    🐛 LimitedHTMLRenderer compat for python<3.4

--- contrib/limited_html_renderer.py
+++ contrib/limited_html_renderer.py
@@ -1,4 +1,8 @@
-import html
+import sys
+if sys.version_info < (3, 4):
+    from mistletoe import _html as html
+else:
+    import html
 from mistletoe.html_renderer import HTMLRenderer
 
 class LimitedHTMLRenderer(HTMLRenderer):
