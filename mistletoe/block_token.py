aa823d215368efca2b913a2a530aa07ac4c399ff
Grollicus <Grollicus@users.noreply.github.com>
2019-06-08 19:23:09 +0200

    Tables can handle rows with missing columns (#67)

--- mistletoe/block_token.py
+++ mistletoe/block_token.py
@@ -673,7 +673,7 @@ class TableRow(BlockToken):
     def __init__(self, line, row_align=None):
         self.row_align = row_align or [None]
         cells = filter(None, line.strip().split('|'))
-        self.children = [TableCell(cell.strip(), align)
+        self.children = [TableCell(cell.strip() if cell else '', align)
                          for cell, align in zip_longest(cells, self.row_align)]
 
 
