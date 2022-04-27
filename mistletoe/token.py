a24a6f10302f7cd9b173fa0571ea5f59f306689e
Walter Doerwald <walter@livinglogic.de>
2022-04-27 10:14:54 +0200

    Add markup to docstrings in token module.

--- mistletoe/token.py
+++ mistletoe/token.py
@@ -5,7 +5,7 @@ Base token class.
 
 def _short_repr(value):
     """
-    Return a shortened repr output of value for use in __repr__ method.
+    Return a shortened ``repr`` output of value for use in ``__repr__`` methods.
     """
 
     if isinstance(value, str):
@@ -20,22 +20,22 @@ class Token:
     """
     Base token class.
 
-    Token has two subclasses:
+    `Token` has two subclasses:
 
-    * block_token.BlockToken, for all block level tokens. A block level token
+    * `block_token.BlockToken`, for all block level tokens. A block level token
       is text which occupies the entire horizontal width of the "page" and is
       offset for the surrounding sibling block with line breaks.
 
-    * span_token.SpanToken, for all span-level (or inline-level) tokens.
+    * `span_token.SpanToken`, for all span-level (or inline-level) tokens.
       A span-level token appears inside the flow of the text lines without any
       surrounding line break.
 
-    Custom __repr__ methods in subclasses: The default __repr__ implementation
-    outputs the number of child tokens (from the attribute children) if
-    applicable, and the content attribute if applicable. If any additional
-    attributes should be included in the __repr__ output, this can be specified
-    by setting the class attribute repr_attributes to a tuple containing the
-    attribute names to be output.
+    Custom ``__repr__`` methods in subclasses: The default ``__repr__``
+    implementation outputs the number of child tokens (from the attribute
+    ``children``) if applicable, and the ``content`` attribute if applicable.
+    If any additional attributes should be included in the ``__repr__`` output,
+    this can be specified by setting the class attribute ``repr_attributes``
+    to a tuple containing the attribute names to be output.
     """
 
     repr_attributes = ()
