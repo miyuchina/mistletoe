ce7d81d7df85df4c391a8045b51ebd679dc8a468
Anders Kaplan <anders.kaplan@magasin1.se>
2022-08-09 18:16:56 +0200

    Switched to use the published spec.json instead of cloning CommonMark to extract it.

--- test/specification/spec.sh
+++ test/specification/spec.sh
@@ -2,23 +2,13 @@
 
 set -e
 
-REPO="https://github.com/commonmark/CommonMark.git"
 VERSION="0.28"
+URL="https://spec.commonmark.org/$VERSION/spec.json"
 
 function main {
-    echo "Cloning from repo: $REPO..."
-    git clone --quiet $REPO
-
     echo "Using version $VERSION..."
-    cd "CommonMark"
-    git checkout --quiet $VERSION
 
-    echo "Dumping tests file..."
-    python3 "test/spec_tests.py" --dump-tests > "../commonmark.json"
-
-    echo "Cleaning up..."
-    cd ..
-    rm -rf CommonMark
+    curl -k -o commonmark.json $URL
 
     echo "Done."
 }
