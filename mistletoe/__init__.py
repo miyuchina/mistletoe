miyuchina/mistletoe

commit aacc0b775ee2205c7017b7599aae9b1184c41c91

refactor: use strict PascalCase for class names (#182)
This makes it more practical when compound class
names are introduced.

For backwards compatibility, we also introduce aliases
for the old names, marking them as deprecated.

mistletoe/__init__.py
@@ -7,12 +7,12 @@ __all__ = ['html_renderer', 'ast_renderer', 'block_token', 'block_tokenizer',
            'span_token', 'span_tokenizer']
 
 from mistletoe.block_token import Document
-from mistletoe.html_renderer import HTMLRenderer
+from mistletoe.html_renderer import HtmlRenderer
 
-def markdown(iterable, renderer=HTMLRenderer):
+def markdown(iterable, renderer=HtmlRenderer):
     """
     Converts markdown input to the output supported by the given renderer.
-    If no renderer is supplied, ``HTMLRenderer`` is used.
+    If no renderer is supplied, ``HtmlRenderer`` is used.
 
     Note that extra token types supported by the given renderer
     are automatically (and temporarily) added to the parsing process.

docs/__init__.py
@@ -1,4 +1,4 @@
-from mistletoe import Document, HTMLRenderer, __version__
+from mistletoe import Document, HtmlRenderer, __version__
 
 INCLUDE = {'README.md': 'index.html',
            'CONTRIBUTING.md': 'contributing.html'}
@@ -19,7 +19,7 @@ METADATA = """
 """
 
 
-class DocRenderer(HTMLRenderer):
+class DocRenderer(HtmlRenderer):
     def render_link(self, token):
         return super().render_link(self._replace_link(token))
 

docs/index.html
@@ -81,14 +81,14 @@ with open(&#x27;foo.md&#x27;, &#x27;r&#x27;) as fin:
     rendered = mistletoe.markdown(fin, LaTeXRenderer)
 </code></pre>
 <p>Finally, here&#x27;s how you would manually specify extra tokens and a renderer
-for mistletoe. In the following example, we use <code>HTMLRenderer</code> to render
-the AST, which adds <code>HTMLBlock</code> and <code>HTMLSpan</code> to the normal parsing
+for mistletoe. In the following example, we use <code>HtmlRenderer</code> to render
+the AST, which adds <code>HtmlBlock</code> and <code>HtmlSpan</code> to the normal parsing
 process.
 </p>
-<pre><code class="lang-python">from mistletoe import Document, HTMLRenderer
+<pre><code class="lang-python">from mistletoe import Document, HtmlRenderer
 
 with open(&#x27;foo.md&#x27;, &#x27;r&#x27;) as fin:
-    with HTMLRenderer() as renderer:
+    with HtmlRenderer() as renderer:
         rendered = renderer.render(Document(fin))
 </code></pre>
 <h3>From the command-line</h3>
@@ -209,9 +209,9 @@ of nasty implementation details. Fortunately, mistletoe takes care
 of most of them for you. Simply pass your custom token class to 
 <code>super().__init__()</code> does the trick:
 </p>
-<pre><code class="lang-python">from mistletoe.html_renderer import HTMLRenderer
+<pre><code class="lang-python">from mistletoe.html_renderer import HtmlRenderer
 
-class GithubWikiRenderer(HTMLRenderer):
+class GithubWikiRenderer(HtmlRenderer):
     def __init__(self):
         super().__init__(GithubWiki)
 </code></pre>
@@ -225,9 +225,9 @@ class GithubWikiRenderer(HTMLRenderer):
 </code></pre>
 <p>Cleaning up, we have our new renderer class:
 </p>
-<pre><code class="lang-python">from mistletoe.html_renderer import HTMLRenderer, escape_url
+<pre><code class="lang-python">from mistletoe.html_renderer import HtmlRenderer, escape_url
 
-class GithubWikiRenderer(HTMLRenderer):
+class GithubWikiRenderer(HtmlRenderer):
     def __init__(self):
         super().__init__(GithubWiki)
 

mistletoe/ast_renderer.py
@@ -5,7 +5,7 @@ Abstract syntax tree renderer for mistletoe.
 import json
 from mistletoe.base_renderer import BaseRenderer
 
-class ASTRenderer(BaseRenderer):
+class AstRenderer(BaseRenderer):
     def render(self, token):
         """
         Returns the string representation of the AST.
@@ -43,3 +43,9 @@ def get_ast(token):
     if 'children' in vars(token):
         node['children'] = [get_ast(child) for child in token.children]
     return node
+
+
+ASTRenderer = AstRenderer
+"""
+Deprecated name of the `AstRenderer` class.
+"""

mistletoe/block_token.py
@@ -997,7 +997,7 @@ class ThematicBreak(BlockToken):
         return [next(lines)]
 
 
-class HTMLBlock(BlockToken):
+class HtmlBlock(BlockToken):
     """
     Block-level HTML token.
     This is a leaf block token with a single child of type span_token.RawText,
@@ -1074,6 +1074,12 @@ class HTMLBlock(BlockToken):
                 lines.backstep()
                 break
         return line_buffer
+
+
+HTMLBlock = HtmlBlock
+"""
+Deprecated name of the `HtmlBlock` class.
+"""
 
 
 _token_types = []

mistletoe/cli.py
@@ -56,7 +56,7 @@ def interactive(renderer):
 def parse(args):
     parser = ArgumentParser()
     parser.add_argument('-r', '--renderer', type=_import,
-                        default='mistletoe.HTMLRenderer',
+                        default='mistletoe.HtmlRenderer',
                         help='specify an importable renderer class')
     parser.add_argument('-v', '--version', action='version', version=version_str)
     parser.add_argument('filenames', nargs='*',
@@ -88,6 +88,6 @@ def _import_readline():
 def _print_heading(renderer):
     print('{} (interactive)'.format(version_str))
     print('Type Ctrl-D to complete input, or Ctrl-C to exit.')
-    if renderer is not mistletoe.HTMLRenderer:
+    if renderer is not mistletoe.HtmlRenderer:
         print('Using renderer: {}'.format(renderer.__name__))
 

mistletoe/contrib/github_wiki.py
@@ -4,7 +4,7 @@ GitHub Wiki support for mistletoe.
 
 import re
 from mistletoe.span_token import SpanToken
-from mistletoe.html_renderer import HTMLRenderer
+from mistletoe.html_renderer import HtmlRenderer
 
 
 __all__ = ['GithubWiki', 'GithubWikiRenderer']
@@ -17,7 +17,7 @@ class GithubWiki(SpanToken):
         self.target = match.group(2)
 
 
-class GithubWikiRenderer(HTMLRenderer):
+class GithubWikiRenderer(HtmlRenderer):
     def __init__(self, **kwargs):
         """
         Args:

mistletoe/contrib/jira_renderer.py
@@ -26,7 +26,7 @@ from mistletoe import block_token, span_token
 from mistletoe.base_renderer import BaseRenderer
 import re
 
-class JIRARenderer(BaseRenderer):
+class JiraRenderer(BaseRenderer):
     """
     JIRA renderer class.
 
@@ -39,7 +39,7 @@ class JIRARenderer(BaseRenderer):
         """
         self.listTokens = []
         self.lastChildOfQuotes = []
-        super().__init__(*chain([block_token.HTMLBlock, span_token.HTMLSpan], extras))
+        super().__init__(*chain([block_token.HtmlBlock, span_token.HtmlSpan], extras))
 
     def render_strong(self, token):
         template = '*{}*'
@@ -233,4 +233,8 @@ def escape_url(raw):
     from urllib.parse import quote
     return quote(raw, safe='/#:()*?=%@+,&;')
 
-    
+
+JIRARenderer = JiraRenderer
+"""
+Deprecated name of the `JiraRenderer` class.
+"""

commit b02b6a12e8a08fd7de389cf070291d53e01b33c5

fix: make `mistletoe.HTMLRenderer` work again
Backwards compatibility was actually broken in #182 inadvertently -
it didn't cover the case when the old `HTMLRenderer` was imported directly
from the `mistletoe` package.

Note: The `# noqa: F401` comment is for the flake8 linter, it suppresses
the `imported but unused` rule.

mistletoe/__init__.py
@@ -8,6 +8,9 @@ __all__ = ['html_renderer', 'ast_renderer', 'block_token', 'block_tokenizer',
 
 from mistletoe.block_token import Document
 from mistletoe.html_renderer import HtmlRenderer
+# import the old name for backwards compatibility:
+from mistletoe.html_renderer import HTMLRenderer  # noqa: F401
+
 
 def markdown(iterable, renderer=HtmlRenderer):
     """