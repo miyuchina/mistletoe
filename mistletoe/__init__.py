db7505532587af7d68140f7976871d3ca825e315
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-10-14 02:50:07 -0400

    Add types to __init__.py's markdown function (#251)
    pyright returns an error when there are no type annotations if using any other `renderer` besides `HtmlRenderer`, because it assumes the type is `type[HtmlRenderer]` instead of `type[BaseRenderer]` like it should be.
    
    To stay compatible with Python 3.5, I've used things from the `typing` module (also used elsewhere in the codebase).

--- mistletoe/__init__.py
+++ mistletoe/__init__.py
@@ -6,13 +6,16 @@ __version__ = "1.5.0.dev"
 __all__ = ['html_renderer', 'ast_renderer', 'block_token', 'block_tokenizer',
            'span_token', 'span_tokenizer']
 
+from typing import Callable, Iterable, Union
+
+from mistletoe.base_renderer import BaseRenderer
 from mistletoe.block_token import Document
 from mistletoe.html_renderer import HtmlRenderer
 # import the old name for backwards compatibility:
 from mistletoe.html_renderer import HTMLRenderer  # noqa: F401
 
 
-def markdown(iterable, renderer=HtmlRenderer):
+def markdown(iterable: Union[str, Iterable[str]], renderer: Callable[..., BaseRenderer] = HtmlRenderer):
     """
     Converts markdown input to the output supported by the given renderer.
     If no renderer is supplied, ``HtmlRenderer`` is used.
@@ -20,5 +23,5 @@ def markdown(iterable, renderer=HtmlRenderer):
     Note that extra token types supported by the given renderer
     are automatically (and temporarily) added to the parsing process.
     """
-    with renderer() as renderer:
-        return renderer.render(Document(iterable))
+    with renderer() as r:
+        return r.render(Document(iterable))
