0860a8662d386b0b9bee2512e8a078715703f980
Petr Bodnar <p.bodnar@centrum.cz>
2025-10-18 18:43:52 +0200

    chore: next dev version

--- mistletoe/__init__.py
+++ mistletoe/__init__.py
@@ -2,7 +2,7 @@
 Make mistletoe easier to import.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.6.0.dev"
 __all__ = ['html_renderer', 'ast_renderer', 'block_token', 'block_tokenizer',
            'span_token', 'span_tokenizer']
 
