b43f5cbed505286e9fecfca914e033320a703faf
наб <nabijaczleweli@nabijaczleweli.xyz>
2026-01-11 15:15:47 +0100

    Fix <http://example.com/@user/1234> being rendered as <a href="mailto:...">...</a>
    Closes #259

--- test/test_repr.py
+++ test/test_repr.py
@@ -103,6 +103,12 @@ class TestRepr(unittest.TestCase):
         doc = Document("Foo <http://www.example.org/>\n")
         self._check_repr_matches(doc.children[0].children[1], "span_token.AutoLink with 1 child target='http://www.example.org/' mailto=False")
 
+        doc = Document("Foo <user@example.org>\n")
+        self._check_repr_matches(doc.children[0].children[1], "span_token.AutoLink with 1 child target='user@example.org' mailto=True")
+
+        doc = Document("Foo <http://example.org/@user/1234>\n")
+        self._check_repr_matches(doc.children[0].children[1], "span_token.AutoLink with 1 child target='http://example.org/@user/1234' mailto=False")
+
     def test_escapesequence(self):
         doc = Document("\\*\n")
         self._check_repr_matches(doc.children[0].children[0], "span_token.EscapeSequence with 1 child")
