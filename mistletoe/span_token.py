1efc2994705e0a00661ef16eacf91429917b31fa
наб <nabijaczleweli@nabijaczleweli.xyz>
2026-01-11 15:15:47 +0100

    Fix <http://example.com/@user/1234> being rendered as <a href="mailto:...">...</a>
    Closes #259

--- mistletoe/span_token.py
+++ mistletoe/span_token.py
@@ -192,7 +192,7 @@ class Link(SpanToken):
 
 class AutoLink(SpanToken):
     """
-    Autolink token. ("<http://www.google.com>")
+    Autolink token. ("<http://www.google.com>", or "<user@example.org>")
     This is an inline token with a single child of type RawText.
 
     Attributes:
@@ -201,14 +201,15 @@ class AutoLink(SpanToken):
         mailto (bool): true iff the target looks like an email address, but does not have the "mailto:" prefix.
     """
     repr_attributes = ("target", "mailto")
-    pattern = re.compile(r"(?<!\\)(?:\\\\)*<([A-Za-z][A-Za-z0-9+.-]{1,31}:[^ <>]*?|[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?(?:\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*)>")
+    pattern = re.compile(r"(?<!\\)(?:\\\\)*<(([A-Za-z][A-Za-z0-9+.-]{1,31}):[^ <>]*?|[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?(?:\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*)>")
     parse_inner = False
 
     def __init__(self, match):
         content = match.group(self.parse_group)
+        scheme = match.group(2)
         self.children = (RawText(content),)
         self.target = content
-        self.mailto = '@' in self.target and 'mailto' not in self.target.casefold()
+        self.mailto = not scheme
 
 
 class EscapeSequence(SpanToken):
