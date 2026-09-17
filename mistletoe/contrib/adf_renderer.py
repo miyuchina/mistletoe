1d99ce6e8270be0529912e846d854f3f0d9db795
APS1990A <a.p.setiawan90@outlook.com>
2026-09-17 22:44:14 +0700

    Refactor logic to use similar structure to get_ast

--- mistletoe/contrib/adf_renderer.py
+++ mistletoe/contrib/adf_renderer.py
@@ -1,87 +1,120 @@
+"""
+Abstract syntax tree renderer for mistletoe.
+"""
+
 import json
-
 from mistletoe.base_renderer import BaseRenderer
-from mistletoe.span_token import Emphasis, LineBreak, Strikethrough, Strong
 
 
-class ADFRenderer(BaseRenderer):
+def determine_list_type(token, node):
+    if token.children:
+        leader: str = getattr(token.children[0], "leader").replace(".", "")
+        if leader.isdecimal():
+            node['attrs'] = {}
+            node['attrs']['order'] = leader
+            return "orderedList"
+        
 
-    ADF_VERSION = 1
-    current_marks = []
+    return "bulletList"
 
-    def __init__(self):
-        self.adf_func = {
-            "Document": self.adf_doc_node,
-            "RawText": self.adf_text_node,
-            "Heading": self.adf_heading_node,
-            "Paragraph": self.adf_paragraph_node,
-            "Emphasis": self.adf_marks
-        }
 
-    def adf_doc_node(self, token):
-        return {
-            "type": "doc",
-            "version": self.ADF_VERSION,
-            "content": self.get_child_nodes(token),
-        }
+ADF_TYPE = {
+    "Document": "doc",
+    "Heading": "heading",
+    "RawText": "text",
+    "Paragraph": "paragraph",
+    "Emphasis": "em",
+    "Strong": "strong",
+    "Strikethrough": "strike",
+    "LineBreak": None,
+    "Link": "link",
+    "InlineCode": "code",
+    "CodeFence": "codeBlock",
+    "List": determine_list_type,
+    "ListItem": "listItem",
+}
 
-    def adf_marks(self, token):
-        self.current_marks.append({"type": self.discover_mark_type(token)})
-        return self.get_child_nodes(token)
+ADF_ATTRS = {
+    "level": "level",
+    "target": "href",
+    "title": "title",
+    "language": "language",
+}
+MARK_VALUES = ("em", "strong", "strike", "link", "code")
 
-    def adf_heading_node(self, token):
-        return {
-            "type": "heading",
-            "attrs": {"level": getattr(token, "level")},
-            "content": self.get_child_nodes(token),
-        }
 
-    def adf_paragraph_node(self, token):
-        return {"type": "paragraph", "content": self.get_child_nodes(token)}
-
-    def get_child_nodes(self, token):
-        return [
-            self.adf_func[child.__class__.__name__](child)
-            for child in getattr(token, "children")
-            if not isinstance(child, (LineBreak, Emphasis))
-        ]
-
-    def adf_text_node(self, token):
-        node = {}
-        node["type"] = "text"
-        node["text"] = getattr(token, "content").replace("\n", " ")
-        node.update(self.adf_span_node_preprocess())
-
-        return node
-
-    def adf_span_node_preprocess(self):
-        node = {}
-        node["marks"] = [mark for mark in self.current_marks]
-        self.current_marks = []
-        return node
-
-    def discover_mark_type(self, token):
-        if isinstance(token, Emphasis):
-            return "em"
-        if isinstance(token, Strong):
-            return "strong"
-        if isinstance(token, Strikethrough):
-            return "strike"
-
+class AdfRenderer(BaseRenderer):
     def render(self, token):
         """
         Returns the string representation of the ADF.
 
-        Overrides super().render. Delegates the logic to get_adf.
+        Overrides super().render. Delegates the logic to get_adf       
         """
-
-        return json.dumps(get_adf(token, ADFRenderer()), indent=2) + "\n"
+        return json.dumps(get_adt(token), indent=2) + "\n"
 
     def __getattr__(self, name):
         return lambda token: ""
 
 
+def get_adt(token, marks=None):
+    """
+    Recursively unrolls token attributes into dictionaries (token.children
+    into lists).
 
-def get_adf(token, renderer):
-    node = renderer.adf_func[token.__class__.__name__](token)
+    Returns:
+        a dictionary of token's attributes.
+    """
+    node = {}
+    # Python 3.6 uses [ordered dicts] [1].
+    # Put in 'type' entry first to make the final tree format somewhat
+    # similar to [MDAST] [2].
+    #
+    #   [1]: https://docs.python.org/3/whatsNonenew/3.6.html
+    #   [2]: https://github.com/syntax-tree/mdast
+    node["type"] = (
+       (ADF_TYPE[token.__class__.__name__])(token, node)
+        if callable(ADF_TYPE[token.__class__.__name__])
+        else ADF_TYPE[token.__class__.__name__]
+    )
+
+    if node["type"] is not None:
+        if node["type"] == "doc":
+            node["version"] = 1
+        elif node["type"] != "orderedList":
+            node["attrs"] = {}
+
+        if "content" in vars(token):
+            node["text"] = getattr(token, "content").replace("\n", "")
+        for attrname in token.repr_attributes:
+            if ADF_ATTRS.get(attrname, None) is not None:
+                node["attrs"] = {} if node.get("attrs", None) is None else node["attrs"]
+                node["attrs"][ADF_ATTRS[attrname]] = getattr(token, attrname)
+        if node["type"] in MARK_VALUES:
+            marks = [] if marks is None else marks
+            if node.get("attrs", None) is not None:
+                if len(node["attrs"]) == 0:
+                    del node["attrs"]
+            marks.append(node)
+            return get_adt(token.children[0], marks)
+        if "header" in vars(token):
+            node["header"] = get_adt(getattr(token, "header"))
+        if token.children is not None:
+            node["content"] = [
+                get_adt(child)
+                for child in token.children
+                if ADF_TYPE[child.__class__.__name__]
+            ]
+    else:
+        if token.children is not None:
+            return [
+                get_adt(child)
+                for child in token.children
+                if ADF_TYPE[child.__class__.__name__]
+            ]
+
+    if node.get("attrs", None) is not None:
+        if len(node["attrs"]) == 0:
+            del node["attrs"]
+    if marks is not None and len(marks) > 0:
+        node["marks"] = marks
     return node
